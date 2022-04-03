20abfaaa5b8d7fe164b8ca8510dbca330ba736b4
jayant <jayantparmar420@gmail.com>
2022-04-03 09:05:46 +0530

    Added valid first name using test cases.

--- src/main/java/UserRegistration.java
+++ src/main/java/UserRegistration.java
@@ -1,2 +1,29 @@
+/**
+ * As a User need to enter a valid First Name
+ * - First name starts with Cap and has minimum 3 characters
+ * @author user-Almas
+ *
+ */
+
+/**
+ * created class name as UserRegistration
+ *
+ */
 public class UserRegistration {
+    /**
+     * created method checkFName and passing parameter in this it will check
+     * FirstName is valid or not result will be in true or false because method is
+     * boolean type
+     *
+     * @param fName -FirstName of the String
+     * @return -return to method created
+     */
+    public boolean checkFName(String fName) {
+        /**
+         * regex pattern for FirstName
+         */
+        return (fName.matches("[A-Z][a-z]{3,}"));
+
+    }
+
 }
